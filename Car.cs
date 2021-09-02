15cd6124a1fa95e076e83713aab7359900553d6a
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-02 08:13:26 +0530

    Added

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        string color = "red";
+        string model = "swift";
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        string color = "red";
+        string model = "swift";
+        int model = 123456;
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+        public void Drive()
+        {
+            Console.WriteLine(Driving);
+        }
+        public static void Brake()
+        {
+            Console.WriteLine("brake")
+        }
+        static void Main(string{[]args)
+
+    }
+}
